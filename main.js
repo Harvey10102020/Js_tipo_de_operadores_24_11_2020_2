1f656ee7f2de8831a79bb0681c638bff79faab30
soporte <=>
2020-11-24 21:04:05 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+let numero_1 = 15;
+let numero_2 = 3;
+numero_1++; //incremento de un número
+numero_1 += 2 //incremento de número la cantidad necesaria 2
+console.log(numero_1);
+
+
+numero_1--; //decremento de un número
+numero_1 -= 2 //decremento de número la cantidad necesaria 2
+console.log(numero_1);
+
+resultado = numero_1 + numero_2; //suma ambos números
+console.log(resultado);
+
+multiplicacion = numero_1 * numero_2;
+console.log(multiplicacion);
+
+division = numero_1 / numero_2;
+console.log(division);
+
+//Residuo de Número 
+numero_1 %= numero_2;
+console.log(numero_1);
+
+if (numero_1 == 0) {
+
+    console.log("El Residuo es Cero ");
+
+}
+
+
+// // Asignación
+// let numeroA = 15;
+// let numeroB = 25;
+// numeroA = numeroB;
+// console.log(numeroA);
+
+// // Adición
+// let numero1 = 10;
+// let numero2 = 25;
+// resultado = numero2 += numero1;
+// console.log(resultado);
+
+
+// // Sustracción
+// resultado = numero2 -= numero1;
+// console.log(resultado);
+
+// // Multiplicación
+// resultado = numero2 *= numero1;
+// console.log(resultado);
+
+
+// // División
+// resultado = numero2 /= numero1;
+// console.log(resultado);
+
+
+// // Resto
+// resultado = numero2 %= numero1;
+// console.log(resultado);
+
+
+// // Exponencial
+// numero2 **= numero1;
+// console.log(numero2);
